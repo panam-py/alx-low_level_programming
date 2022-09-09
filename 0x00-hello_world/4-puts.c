518e18c049ad76338a1fe290e466c2d8b52d7dd7
panam-py <panampraisehebron@gmail.com>
2022-09-09 04:38:28 +0100

    [update]: added new tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - Start point
+ * Return: 0 (positve) or 1 (negative)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0); }
