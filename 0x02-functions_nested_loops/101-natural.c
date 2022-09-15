b7c58330038aeb858ca7225bda8cb03fb8b49ed1
panam-py <panampraisehebron@gmail.com>
2022-09-15 15:26:21 +0100

    [update]: added task to find the sum of all multiples of 3 0r 5 before 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints and returns sum of multiples of 3, 5 before 1024
+ *
+ * Return: int, sum of multiples of 3, 5, before 1024
+*/
+int main (void)
+{
+int i, sum;
+sum = 0;
+for (i = 1; i < 1024; i++)
+{
+if (i % 3 == 0 || i % 5 == 0)
+sum += i;
+}
+printf("%d", sum);
+return (0);
+}
