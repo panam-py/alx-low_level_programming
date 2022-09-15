4547ebe03a2e662667bbac5354cd0d0ee92f837c
panam-py <panampraisehebron@gmail.com>
2022-09-15 14:19:04 +0100

    [update]: added code to add tow integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - Adds up two integers and returns result
+ * @i: THe first integer
+ * @j: The second integer
+ * Return: int, the addition of i and j
+*/
+int add(int i, int j)
+{
+return (i + j);
+}
