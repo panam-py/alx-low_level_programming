0ca1aab430d55f42d91d672dc5d10071514806e7
panam-py <panampraisehebron@gmail.com>
2022-09-15 19:22:30 +0100

    [update]: added fizzbuzz task:

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main- Prints numbers from 1 - 100, denoting multiples of 3 as Fizz
+ * and multiples of 5 as Buzz and both as FizzBuzz
+ *
+ * Return: void
+*/
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+printf(", FizzBuzz");
+else if (i % 3 == 0)
+printf(", Fizz");
+else if (i % 5 == 0)
+printf(", Buzz");
+else{
+if (i == 1)
+printf("%d", i);
+else
+printf(", %d", i);
+}
+}
+return (0);
+}
