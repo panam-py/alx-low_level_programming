463cc32973367e84bffa164fa0437c38e812f481
panam-py <panampraisehebron@gmail.com>
2022-09-15 17:08:50 +0100

    [update]: added task to print all digits

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints all digits from 0 - 9
+ *
+ * Return: void
+*/
+void print_numbers(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+_putchar(i);
+_putchar('\n');
+}
