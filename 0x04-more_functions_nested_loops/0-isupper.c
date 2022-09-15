2557c43b680086c518df4627796ffabb6d09c4c2
panam-py <panampraisehebron@gmail.com>
2022-09-15 16:51:00 +0100

    [update]: added _putchar function, function to check if character is uppercase and main header file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks if a letter is uppercase
+ * @c: Letter to check
+ * Return: 1 if letter is uppercase, 0 otherwise 
+*/
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+return (1);
+else
+return (0);
+}
