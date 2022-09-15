4afb755859a49f27413c42b91151889b107a692f
panam-py <panampraisehebron@gmail.com>
2022-09-15 13:48:01 +0100

    [update]: added task to get the absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - Returns the absolut value of a number
+ * @i: The number to be entered
+ * Return: int, The absolute value of number entered
+*/
+int _abs(int i)
+{
+if (i > 0)
+return (i);
+else
+return (i * -1);
+}
