e7e7fa51d24a0b37e2ffb2fae77d60b03eba9716
panam-py <panampraisehebron@gmail.com>
2022-09-15 16:59:09 +0100

    [update]: added task to check if char is a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - Checks is a number is a digit
+ * @c: Digit to check
+ * Return: 1 if c is digit, 0 otherwise
+*/
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 56)
+return (1);
+else
+return (0);
+}
