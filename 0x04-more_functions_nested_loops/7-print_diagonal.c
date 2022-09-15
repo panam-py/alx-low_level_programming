417314f777356ebe1eac84b86b15c476161fbe04
panam-py <panampraisehebron@gmail.com>
2022-09-15 18:07:37 +0100

    [update]: added task to print reverse slash

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Prints "\" certain amount of times
+ * @n: Amount of times to print
+ * Return: void
+*/
+void print_diagonal(int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar(92);
+}
+_putchar('\n');
+}
