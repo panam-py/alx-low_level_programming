594a10a0d280d4f0d31fe8537945419878ace7c3
panam-py <panampraisehebron@gmail.com>
2022-09-15 12:49:34 +0100

    [update]: added task to print the alphabet in lower case

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+  * print_alphabet- Print the alphabet in lower case
+  *
+  * Return 0 (Always success)
+*/
+void print_alphabet(void)
+{
+int i;
+for (i = 97; i <= 122; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
