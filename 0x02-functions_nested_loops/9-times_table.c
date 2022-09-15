605bf8db8407da14cae9b6acfee2ad7793950a69
panam-py <panampraisehebron@gmail.com>
2022-09-15 15:17:09 +0100

    [fix]: added extra spacing for smaller numbers

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,28 +3,30 @@
 /**
  * times_table - Prints the 9 times table
  *
- * Return: void
-*/
+ * Return: no return
+ */
 void times_table(void)
 {
-int a, b, ans;
+int a, b, op;
+
 for (a = 0; a <= 9; a++)
 {
-for (b = 0; b <= 9; b++)
+_putchar(48);
+for (b = 1; b <= 9; b++)
 {
-ans = a * b;
-if (ans <= 9)
+op = a * b;
+_putchar(44);
+_putchar(32);
+if (op <= 9)
 {
 _putchar(32);
-_putchar(ans + 48);
+_putchar(op + 48);
 }
 else
 {
-_putchar((ans / 10) + 48);
-_putchar((ans % 10) + 48);
+_putchar((op / 10) + 48);
+_putchar((op % 10) + 48);
 }
-_putchar(44);
-_putchar(32);
 }
 _putchar('\n');
 }
