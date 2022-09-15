4c29f85adbfbd93c1a40568d653af09db80a5d78
panam-py <panampraisehebron@gmail.com>
2022-09-15 15:38:59 +0100

    [update]: added task to calculate first 50 fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main- Prints the first 50 fibonacci numbers
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+int first, second, fib, count;
+first = 1;
+second = 2;
+printf("%d, %d, ", first, second);
+for (count = 1; count <= 48; count ++)
+{
+fib = first + second;
+printf("%d, ", fib);
+first = second;
+second = fib;
+}
+printf("\n");
+return (0);
+}
