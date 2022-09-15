eccaafdde41cc7a8f8938b634a090e3588bb83bb
panam-py <panampraisehebron@gmail.com>
2022-09-15 17:03:35 +0100

    [update]: added task to multiply numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - Multplies two integers
+ * @a: First integer to multiply
+ * @b: Second integer to multiply
+ * Return: the result of the multiplication of a and b
+*/
+int mul(int a, int b)
+{
+return (a * b);
+}
