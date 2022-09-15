252765c0fa12b87b748ca1c47f1801ecb0b5fe7d
panam-py <panampraisehebron@gmail.com>
2022-09-15 17:29:42 +0100

    [fix]: made corrections

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,11 +8,14 @@
 void more_numbers(void)
 {
 int i, j;
-for (i = 0; i <= 14; i++)
+for (i = 1; i <= 10; i++)
 {
-j = 10 * i;
+for (j = 0; j <= 14; j++)
+{
+if (j > 9)
 _putchar((j / 10) + 48);
 _putchar((j % 10) + 48);
 }
 _putchar('\n');
+}
 }
