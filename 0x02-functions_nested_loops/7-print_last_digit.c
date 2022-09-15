a79e8102573b6f6c607d06d267043e7b4f41bcd9
panam-py <panampraisehebron@gmail.com>
2022-09-15 13:59:39 +0100

    [update]: added task to check the last digit of a number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Returns the last digit of an integer
+ * @i: The integer passed
+ * Return: int, the last digit in number
+*/
+int print_last_digit(int i)
+{
+return (i % 10);
+}
