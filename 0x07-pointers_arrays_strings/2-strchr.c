5aca39c96546e2207e9447cc4031b0e2314fb98b
panam-py <panampraisehebron@gmail.com>
2022-09-28 09:21:37 +0100

    [update]: added tasks for strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - Returns the pointer to the first occurence of character in a string
+ * @s: The string to be searched
+ * @c: The character to be searched fir
+ * Returns: THe pointer to the first occurence of character or NULL
+*/
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+{
+if (*s == c)
+{
+return (s);
+}
+s++;
+}
+if (*s == c)
+{
+return (s);
+}
+return (NULL);
+}
